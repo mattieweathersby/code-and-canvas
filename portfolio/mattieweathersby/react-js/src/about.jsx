28c9cbd9fa5515171fe5ec866fbb9ddf0f3a03e2
mattieweathersby <mweathersby252@gmail.com>
2025-12-04 22:33:10 -0500

    feat:adds aboutme section and created aboutme component

--- portfolio/mattieweathersby/react-js/src/about.jsx
+++ portfolio/mattieweathersby/react-js/src/about.jsx
@@ -9,10 +9,24 @@ const About = () => {
                 <h2 className="contact-title">Contact</h2>
                 <div className="contact-content">
                     <p>mweathersby252@gmail.com</p>
-                    <p>(302)-208-0327</p>
                     <p>https://github.com/mattieweathersby</p>
                     <p>https://www.linkedin.com/in/mattieweathersby/</p>
                 </div>
+            </div>
+            <div className="aboutme-content">
+                <h2 className="aboutme-title">About Me</h2>
+                <div className="aboutme-description">
+                    <p>
+                    Creative and detail-oriented professional transitioning 
+                    into software engineering and UX/UI design, with experience
+                     in brand strategy, user research, and digital design. 
+                     Skilled at turning customer insights into intuitive,
+                      visually engaging solutions that enhance usability 
+                      and drive results. Known for combining empathy with
+                       technical thinking to deliver impactful user experiences.
+                    </p>
+                </div>
+                <h2 className="expereince-title">Experience</h2>
             </div>
         </section>
     )
